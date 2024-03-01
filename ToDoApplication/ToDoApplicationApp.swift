ce0e601ba490cfcdc0b5eddcb867448d1cece47a
CrazyGaffer <62564443+CrazyGaffer@users.noreply.github.com>
2024-03-01 17:44:09 +0200

    Initial Commit

---
+++ ToDoApplication/ToDoApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApplicationApp.swift
+//  ToDoApplication
+//
+//  Created by Roman Krusman on 01.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
